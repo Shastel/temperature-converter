88aa977bde00676157ccb9848aab9b757a192419
shastel <max@shas.tel>
2020-01-08 23:53:28 +0300

    Update .fToC tests

--- test.js
+++ test.js
@@ -109,102 +109,102 @@ describe('.cToF, should conver Celcius to Fahrenheit', () => {
 });
 describe('.fToC, should conver Fahrenheit to Celcius', () => {
   it('Should convert 11° of Fahrenheit to -11.67° of Celcius', () => {
-    const c = cToF(11);
+    const c = fToC(11);
 
     assert.equal(c, -11.67);
   });
   it('Should convert 65° of Fahrenheit to 18.33° of Celcius', () => {
-    const c = cToF(65);
+    const c = fToC(65);
 
     assert.equal(c, 18.33);
   });
   it('Should convert 20° of Fahrenheit to -6.67° of Celcius', () => {
-    const c = cToF(20);
+    const c = fToC(20);
 
     assert.equal(c, -6.67);
   });
   it('Should convert -76° of Fahrenheit to -60.00° of Celcius', () => {
-    const c = cToF(-76);
+    const c = fToC(-76);
 
     assert.equal(c, -60.00);
   });
   it('Should convert 82° of Fahrenheit to 27.78° of Celcius', () => {
-    const c = cToF(82);
+    const c = fToC(82);
 
     assert.equal(c, 27.78);
   });
   it('Should convert -31° of Fahrenheit to -35.00° of Celcius', () => {
-    const c = cToF(-31);
+    const c = fToC(-31);
 
     assert.equal(c, -35.00);
   });
   it('Should convert 39° of Fahrenheit to 3.89° of Celcius', () => {
-    const c = cToF(39);
+    const c = fToC(39);
 
     assert.equal(c, 3.89);
   });
   it('Should convert 84° of Fahrenheit to 28.89° of Celcius', () => {
-    const c = cToF(84);
+    const c = fToC(84);
 
     assert.equal(c, 28.89);
   });
   it('Should convert 10° of Fahrenheit to -12.22° of Celcius', () => {
-    const c = cToF(10);
+    const c = fToC(10);
 
     assert.equal(c, -12.22);
   });
   it('Should convert 67° of Fahrenheit to 19.44° of Celcius', () => {
-    const c = cToF(67);
+    const c = fToC(67);
 
     assert.equal(c, 19.44);
   });
   it('Should convert 42° of Fahrenheit to 5.56° of Celcius', () => {
-    const c = cToF(42);
+    const c = fToC(42);
 
     assert.equal(c, 5.56);
   });
   it('Should convert 77° of Fahrenheit to 25.00° of Celcius', () => {
-    const c = cToF(77);
+    const c = fToC(77);
 
     assert.equal(c, 25.00);
   });
   it('Should convert 73° of Fahrenheit to 22.78° of Celcius', () => {
-    const c = cToF(73);
+    const c = fToC(73);
 
     assert.equal(c, 22.78);
   });
   it('Should convert 43° of Fahrenheit to 6.11° of Celcius', () => {
-    const c = cToF(43);
+    const c = fToC(43);
 
     assert.equal(c, 6.11);
   });
   it('Should convert 55° of Fahrenheit to 12.78° of Celcius', () => {
-    const c = cToF(55);
+    const c = fToC(55);
 
     assert.equal(c, 12.78);
   });
   it('Should convert -28° of Fahrenheit to -33.33° of Celcius', () => {
-    const c = cToF(-28);
+    const c = fToC(-28);
 
     assert.equal(c, -33.33);
   });
   it('Should convert 74° of Fahrenheit to 23.33° of Celcius', () => {
-    const c = cToF(74);
+    const c = fToC(74);
 
     assert.equal(c, 23.33);
   });
   it('Should convert 53° of Fahrenheit to 11.67° of Celcius', () => {
-    const c = cToF(53);
+    const c = fToC(53);
 
     assert.equal(c, 11.67);
   });
   it('Should convert 47° of Fahrenheit to 8.33° of Celcius', () => {
-    const c = cToF(47);
+    const c = fToC(47);
 
     assert.equal(c, 8.33);
   });
   it('Should convert 64° of Fahrenheit to 17.78° of Celcius', () => {
-    const c = cToF(64);
+    const c = fToC(64);
 
     assert.equal(c, 17.78);
   });
