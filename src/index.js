b87b300fcd7545041a91cb078ccd91a7b5a5524c
Dio-Dao <59789616+Dio-Dao@users.noreply.github.com>
2020-05-18 12:00:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 
 exports.cToF = function (c) {
-
+ let tF = 9 / 5 * t + 32;
+  return tF.toFixed(2);
 }
 
 exports.fToC = function (f) {
-
+  let tC = 5 / 9 * (t - 32);
+  return tC.toFixed(2)
 }
